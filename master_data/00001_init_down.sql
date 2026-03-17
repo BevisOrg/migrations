ff7c41d02f64f9c5caf9bebe9be9d1f8cdd4e376
BevisDev / <99348676+BevisDev@users.noreply.github.com>
2026-03-17 22:20:09 +0700

    feat(master_data): 00001 down

---
+++ master_data/00001_init_down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS master_data.priorities;
+DROP TABLE IF EXISTS master_data.statuses;
+DROP TABLE IF EXISTS master_data.issue_types;
+DROP TABLE IF EXISTS master_data.projects;
+DROP SCHEMA IF EXISTS master_data;
